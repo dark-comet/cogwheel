7b3713a6f0afada4477c6c7fee835bb337dbe96c
Haru <kotoshi.no.haru@gmail.com>
2025-07-13 13:33:56 +1200

    Fix Kotlin builder pattern not working
    Can't have another optional parameter after the
    lambda parameter otherwise the caller syntax
    gets convoluted.

--- cogwheel-framework/src/main/kotlin/xyz/darkcomet/cogwheel/framework/primitives/InvocationUtils.kt
+++ cogwheel-framework/src/main/kotlin/xyz/darkcomet/cogwheel/framework/primitives/InvocationUtils.kt
@@ -10,6 +10,30 @@ import kotlinx.coroutines.runBlocking
 import java.util.concurrent.Future
 import java.util.function.Consumer
 
+/*
+    This file defines infrastructure that facilitates API invocations for both Kotlin and Java 
+    callers. It addresses several requirements:
+    
+    - Succinct, idiomatic and seamless calls from Kotlin through the `invoke()` convention + 
+      lambda-with-receiver pattern.
+    
+    - Provide Java callers with an ergonomic way to work with Kotlin's coroutines by providing 
+      `block()` and `async()` calls. These are only intended for Java callers, so use familiar Java
+      library types for parameters.
+          
+    - Provide different flavors of Invocation depending on the number of required parameters. The 
+      general pattern is as follows:
+          - Endpoint URL parameters are mandatory and in method parameters.
+          - Endpoint optional parameters, body params are in TRequestSpec
+          - These methods are `open` for extension so endpoints can override them to provide a more
+            developer-friendly parameter name.
+       
+     Invocations come in two flavors: get-style Invocation which takes mandatory parameters only, 
+     and post-style RequestInvocation which takes a TRequestSpec for building a request body. The 
+     number suffix indicates the number of mandatory parameters in the Invocation.
+ */
+
+// 0 mandatory parameters
 abstract class Invocation0<TResponse> {
     
     abstract suspend operator fun invoke() : Response<TResponse>
@@ -26,6 +50,7 @@ abstract class Invocation0<TResponse> {
 
 }
 
+// 1 mandatory parameter
 abstract class Invocation1<P1, TResponse> {
 
     abstract suspend operator fun invoke(p1: P1) : Response<TResponse>
@@ -42,6 +67,23 @@ abstract class Invocation1<P1, TResponse> {
 
 }
 
+abstract class Invocation2<P1, P2, TResponse> {
+
+    abstract suspend operator fun invoke(p1: P1, p2: P2) : Response<TResponse>
+
+    open fun async(p1: P1, p2: P2) : Future<Response<TResponse>> {
+        return CoroutineScope(Dispatchers.IO)
+            .async { invoke(p1, p2) }
+            .asCompletableFuture()
+    }
+
+    open fun block(p1: P1, p2: P2) : Response<TResponse> {
+        return runBlocking { invoke(p1, p2) }
+    }
+
+}
+
+// no mandatory parameter, optional request spec
 abstract class RequestInvocationS<TRequestSpec, TResponse> {
     
     protected abstract fun createRequest(): TRequestSpec
@@ -50,43 +92,42 @@ abstract class RequestInvocationS<TRequestSpec, TResponse> {
     
     suspend operator fun invoke(
         request: TRequestSpec? = null, 
-        config: Consumer<TRequestSpec>? = null
+        config: (TRequestSpec.() -> Unit)? = null
     ) : Response<TResponse> {
         
         val req = request ?: createRequest()
-        config?.accept(req)
+        config?.invoke(req)
         
         return invoke(req)
     }
 
-    @JvmOverloads
-    open fun async(
-        config: Consumer<TRequestSpec>? = null, 
-        request: TRequestSpec? = null
-    ) : Future<Response<TResponse>> {
-        
-        val req: TRequestSpec = request ?: createRequest()
+    fun async(config: Consumer<TRequestSpec>? = null) : Future<Response<TResponse>> {
+        val req = createRequest()
         config?.accept(req)
 
+        return async(req)
+    }
+    
+    fun async(request: TRequestSpec) : Future<Response<TResponse>> {
         return CoroutineScope(Dispatchers.IO)
-            .async { invoke(req) }
+            .async { invoke(request) }
             .asCompletableFuture()
     }
     
-    @JvmOverloads
-    open fun block(
-        config: Consumer<TRequestSpec>? = null, 
-        request: TRequestSpec? = null
-    ) : Response<TResponse> {
+    fun block(config: Consumer<TRequestSpec>? = null) : Response<TResponse> {
         
-        val req = request ?: createRequest()
+        val req = createRequest()
         config?.accept(req)
 
-        return runBlocking { invoke(req) }
+        return block(req)
+    }
+    
+    fun block(request: TRequestSpec) : Response<TResponse> {
+        return runBlocking { invoke(request) }
     }
 }
 
-// 1 mandatory parameter
+// 1 mandatory parameter + 1 optional request spec
 abstract class RequestInvocation1S<P1, TRequestSpec, TResponse> {
 
     protected abstract fun createRequest(p1: P1): TRequestSpec
@@ -95,45 +136,50 @@ abstract class RequestInvocation1S<P1, TRequestSpec, TResponse> {
     
     open suspend operator fun invoke(
         p1: P1,
-        config: Consumer<TRequestSpec>? = null,
         request: TRequestSpec? = null,
+        config: (TRequestSpec.() -> Unit)? = null
     ) : Response<TResponse> {
         
         val req: TRequestSpec = request ?: createRequest(p1)
-        config?.accept(req)
+        config?.invoke(req)
         
         return invoke(req)
     }
 
-    @JvmOverloads
     open fun async(
         p1: P1,
-        config: Consumer<TRequestSpec>? = null,
-        request: TRequestSpec? = null
+        config: Consumer<TRequestSpec>? = null
     ) : Future<Response<TResponse>> {
         
-        val req: TRequestSpec = request ?: createRequest(p1)
+        val req: TRequestSpec = createRequest(p1)
         config?.accept(req)
 
+        return async(req)
+    }
+    
+    fun async(request: TRequestSpec) : Future<Response<TResponse>> {
         return CoroutineScope(Dispatchers.IO)
-            .async { invoke(req) }
+            .async { invoke(request) }
             .asCompletableFuture()
     }
 
-    @JvmOverloads
     open fun block(
         p1: P1,
-        config: Consumer<TRequestSpec>? = null,
-        request: TRequestSpec? = null
+        config: Consumer<TRequestSpec>? = null
     ) : Response<TResponse> {
         
-        val req: TRequestSpec = request ?: createRequest(p1)
+        val req: TRequestSpec = createRequest(p1)
         config?.accept(req)
 
-        return runBlocking { invoke(req) }
+        return block(req)
+    }
+    
+    fun block(request: TRequestSpec) : Response<TResponse> {
+        return runBlocking { invoke(request) }
     }
 }
 
+// 2 mandatory parameters, no request spec
 abstract class RequestInvocation2<P1, P2, TResponse> {
 
     abstract suspend operator fun invoke(p1: P1, p2: P2) : Response<TResponse>
@@ -150,6 +196,7 @@ abstract class RequestInvocation2<P1, P2, TResponse> {
 
 }
 
+// 2 mandatory parameters + optional request spec
 abstract class RequestInvocation2S<P1, P2, TRequestSpec, TResponse> {
 
     protected abstract fun createRequest(p1: P1, p2: P2): TRequestSpec
@@ -160,42 +207,46 @@ abstract class RequestInvocation2S<P1, P2, TRequestSpec, TResponse> {
         p1: P1, 
         p2: P2,
         request: TRequestSpec? = null,
-        config: Consumer<TRequestSpec>? = null
+        config: (TRequestSpec.() -> Unit)? = null
     ) : Response<TResponse> {
         
         val req: TRequestSpec = request ?: createRequest(p1, p2)
-        config?.accept(req)
+        config?.invoke(req)
 
         return invoke(req)
     }
 
-    @JvmOverloads
     open fun async(
         p1: P1, 
         p2: P2, 
-        config: Consumer<TRequestSpec>? = null, 
-        request: TRequestSpec? = null
+        config: Consumer<TRequestSpec>? = null
     ) : Future<Response<TResponse>> {
     
-        val req: TRequestSpec = request ?: createRequest(p1, p2)
+        val req: TRequestSpec = createRequest(p1, p2)
         config?.accept(req)
         
+        return async(req)
+    }
+    
+    fun async(request: TRequestSpec) : Future<Response<TResponse>> {
         return CoroutineScope(Dispatchers.IO)
-            .async { invoke(req) }
+            .async { invoke(request) }
             .asCompletableFuture()
     }
 
-    @JvmOverloads
     open fun block(
         p1: P1, 
         p2: P2, 
-        config: Consumer<TRequestSpec>? = null, 
-        request: TRequestSpec? = null
+        config: Consumer<TRequestSpec>? = null
     ) : Response<TResponse> {
         
-        val req: TRequestSpec = request ?: createRequest(p1, p2)
+        val req: TRequestSpec = createRequest(p1, p2)
         config?.accept(req)
 
-        return runBlocking { invoke(req) }
+        return block(req)
+    }
+    
+    fun block(request: TRequestSpec) : Response<TResponse> {
+        return runBlocking { invoke(request) }
     }
 }
